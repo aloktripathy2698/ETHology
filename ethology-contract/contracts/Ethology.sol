27dd025749b788d74744eed2ef0f2e56ab8031e5
aloktripathy2698 <aloktripathy242@gmail.com>
2022-04-13 14:22:47 -0400

    Dummy Contract

--- ethology-contract/contracts/Ethology.sol
+++ ethology-contract/contracts/Ethology.sol
@@ -1,5 +1,29 @@
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.6.0
 
-contract Ethology {
-    uint public supplierCount = 0;
+contract hcsc{
+    
+    struct healthcareProvider{
+        uint hcpID;
+    }
+
+    struct product{
+        uint pid;
+        uint pcount;
+        uint pamount;
+    }
+
+    address chairperson;
+    mapping (address => healthcareProvider) hcp;
+    mapping (address => product) public availableProducts;
+    product[] products;
+
+    enum Phase {Init, inStock, out_of _stock}
+    Phase public state = Phase.Init;
+
+    constuctor (uint numberOfProducts) public{
+        chairperson = msg.sender;
+        for (uint i = 0; i < numberOfProducts; i++){
+            products.push(product[0]);
+        }
+    }
 }
